051c30fb159b263742e36bfaef9ac3fa9e8d0666
fahad@thetechidea.com <fahad@thetechidea.com>
2025-04-16 21:20:25 +0300

    update

---
+++ TheTechIdea.Beep.MVVM/ViewModels/BeepConfig/DataSourceDefaultsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheTechIdea.Beep.ConfigUtil;
+
+namespace TheTechIdea.Beep.MVVM.ViewModels.BeepConfig
+{
+    public partial class DataSourceDefaultsViewModel : BaseViewModel
+    {
+        [ObservableProperty]
+        public string dataSourceName;
+        [ObservableProperty]
+        public List<AssemblyClassDefinition> rules;
+        [ObservableProperty]
+        public string selectedRule;
+    }
+}
